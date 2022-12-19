7b546f2fbce2b11e82d0f827cd65877d9c17df5b
Роман <Plotnikovrk@gmail.com>
2022-12-20 00:02:42 +0300

    FirstImpl

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -8,5 +8,15 @@ public class App {
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+
+        HelloWorld myBean = (HelloWorld) applicationContext.getBean("helloworld");
+        Cat catBean1 = applicationContext.getBean("cat", Cat.class);
+        Cat catBean2 = applicationContext.getBean("cat", Cat.class);
+
+        System.out.println(bean == myBean ? "\"Hello world\" beans is equal"
+                : "\"Hello world\" beans are not equal");
+
+        System.out.println(catBean1 == catBean2 ? "\"Cat\" beans is equal"
+                : "\"Cat\" beans are not equal");
     }
 }
---
+++ src/main/java/Cat.java
@@ -0,0 +1,13 @@
+public class Cat {
+
+    private String field;
+
+    public String getField() {
+        return field;
+    }
+
+    public void setField(String field) {
+        this.field = field;
+    }
+}
+
